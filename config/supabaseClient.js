35df80c3fe800f0f0a7f4830aa3e784c87048e1d
anjana-manoj <anjanamanoj117@gmail.com>
2023-11-25 21:07:26 +0530

    abc

--- config/supabaseClient.js
+++ config/supabaseClient.js
@@ -1,8 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl=process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey= process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseAnonkey)
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
